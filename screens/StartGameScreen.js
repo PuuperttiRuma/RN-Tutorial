b348d8de88483632f6748e4da532c8750922137e
Tommi Haapanen <thaapane@student.jyu.fi>
2020-03-14 14:23:03 +0200

    GameScreen opens but no functionality

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -50,7 +50,7 @@ const StartGameScreen = props => {
 			<Card style={styles.summaryContainer}>
 				<Text>You've chosen</Text>
 				<NumberContainer>{selectedNumber}</NumberContainer>
-				<Button title="START GAME" />
+				<Button title="START GAME" onPress={() => props.onStartGame(selectedNumber)} />
 			</Card>
 		);
 	}
