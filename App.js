952d5f6e49dd29951d35d37b6c7a80007f305981
Tommi Haapanen <thaapane@student.jyu.fi>
2020-04-18 23:07:36 +0300

    Game can end w. a ending screen

--- App.js
+++ App.js
@@ -5,18 +5,27 @@ import Header from './components/Header';
 import Todo from './components/Todo';
 import StartGameScreen from './screens/StartGameScreen';
 import GameScreen from './screens/GameScreen';
+import GameOverScreen from './screens/GameOverScreen';
 
 export default function App() {
   const [userNumber, setUserNumber] = useState();
+  const [guessedRounds, setGuessedRounds] = useState(0);
 
   const startGameHandler = (selectedNumber) => {
+    setGuessedRounds(0);
     setUserNumber(selectedNumber);
   }
 
+  const  gameOverHandler = numOfRounds => {
+    setGuessedRounds(numOfRounds);
+  };
+
   let content = <StartGameScreen onStartGame={startGameHandler} />;
 
-  if (userNumber) {
-    content = <GameScreen userChoice={userNumber} />;
+  if (userNumber && guessedRounds <= 0) {
+    content = <GameScreen userChoice={userNumber} onGameOver={gameOverHandler} />;
+  } else if (guessedRounds > 0){
+    content = <GameOverScreen />;
   }
 
   return (
