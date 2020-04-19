f52aa551a3e3c73b94e45477ac9a95514701a712
Tommi Haapanen <thaapane@student.jyu.fi>
2020-04-19 18:18:24 +0300

    Added custom fonts

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { View, StyleSheet } from 'react-native';
+import * as Font from 'expo-font';
+import { AppLoading } from 'expo';
 
 import Header from './components/Header';
 import Todo from './components/Todo';
@@ -7,9 +9,25 @@ import StartGameScreen from './screens/StartGameScreen';
 import GameScreen from './screens/GameScreen';
 import GameOverScreen from './screens/GameOverScreen';
 
+const fetchFonts = () => {
+  return Font.loadAsync({
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
+  });
+}
+
 export default function App() {
   const [userNumber, setUserNumber] = useState();
   const [guessedRounds, setGuessedRounds] = useState(0);
+  const [dataLoaded, setDataLoaded] = useState(false);
+
+  if (!dataLoaded) {
+    return <AppLoading
+      startAsync={fetchFonts}
+      onFinish={() => setDataLoaded(true)}
+      onError={(err) => console.log(err)}
+    />;
+  }
 
   const newGameHandler = () => {
     setUserNumber(null);
@@ -21,7 +39,7 @@ export default function App() {
     setUserNumber(selectedNumber);
   }
 
-  const  gameOverHandler = numOfRounds => {
+  const gameOverHandler = numOfRounds => {
     setGuessedRounds(numOfRounds);
   };
 
@@ -29,17 +47,17 @@ export default function App() {
 
   if (userNumber && guessedRounds <= 0) {
     content = <GameScreen userChoice={userNumber} onGameOver={gameOverHandler} />;
-  } else if (guessedRounds > 0){
-    content = <GameOverScreen 
-      guessedRounds={guessedRounds}  
+  } else if (guessedRounds > 0) {
+    content = <GameOverScreen
+      guessedRounds={guessedRounds}
       userNumber={userNumber}
       onRestart={newGameHandler}
-      />;
+    />;
   }
 
   return (
     <View style={styles.screen}>
-      <Header title="Guess a Number" />      
+      <Header title="Guess a Number" />
       {content}
     </View>
   );
--- components/Header.js
+++ components/Header.js
@@ -2,11 +2,12 @@ import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 
 import Colors from '../constants/colors';
+import TitleText from '../components/TitleText';
 
 const Header = props => {
     return (
         <View style={styles.header}>
-            <Text style={styles.headerTitle}>{props.title}</Text>
+            <TitleText style={styles.headerTitle}>{props.title}</TitleText>
         </View>
     )
 };
@@ -18,11 +19,12 @@ const styles = StyleSheet.create({
         paddingTop: 36,
         backgroundColor: Colors.primary,
         alignItems: 'center',
-        justifyContent: 'center'
+        justifyContent: 'center',        
     },
     headerTitle: {
         color: 'black',
-        fontSize: 18
+        fontSize: 18,
+        fontFamily: 'open-sans-bold',
     }
 });
 
