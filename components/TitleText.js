e33bf421443172760c590396392ba913bc3fe26f
Tommi Haapanen <thaapane@student.jyu.fi>
2020-05-05 14:46:55 +0300

    Stylized the gameover-screen

--- components/TitleText.js
+++ components/TitleText.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { Text, StyleSheet } from 'react-native';
 
 const TitleText = props => {
-  return <Text style={{ ...styles.title, ...props.title }}>{props.children}</Text>
+  return <Text style={{ ...styles.title, ...props.style }}>{props.children}</Text>
 };
 
 const styles = StyleSheet.create({
   title: {
     fontFamily: 'open-sans-bold',
-    fontSize: 18,
+    fontSize: 22,
   }
 });
 
--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,13 +1,33 @@
 import React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import { View, StyleSheet, Text, Button, Image } from 'react-native';
+
+import BodyText from "../components/BodyText";
+import TitleText from "../components/TitleText";
+import Colors from '../constants/colors';
+import colors from '../constants/colors';
 
 const GameOverScreen = props => {
-  return <View style={styles.screen}>
-    <Text>The Game is Over!</Text>
-    <Text>Computer guessed the number in {props.guessedRounds} rounds</Text>
-    <Text>Number to guess was {props.userNumber}</Text>
-    <Button title="Start new Game" onPress={props.onRestart} />
-  </View>
+  return (
+    <View style={styles.screen}>
+      <TitleText>The Game is Over!</TitleText>
+      <View style={styles.imageContainer}>
+        <Image
+          style={styles.image}
+          source={require('../assets/success.png')}
+          //source={{uri: 'https://peda.net/lahti/koulut/tiirismaa/ecol/cnt/sesv/sivu-1/keskustelualue/a-k/mountain-jpg:file/download/09f0a88f6ac933cf73da97071cb3c492c0babc83/mountain.jpg'}}
+          //fadeDuration={3000}
+          resizeMode="cover" />
+      </View>
+      <View style={styles.resultsContainer}>
+        <BodyText style={styles.resultText}>
+          Computer guessed the number <Text style={styles.highlight}>{props.userNumber}</Text> and took <Text style={styles.highlight}>{props.guessedRounds}</Text> rounds to guess it!
+        </BodyText>
+      </View>
+      <View style={styles.buttonContainer}>
+        <Button title="Start new Game" onPress={props.onRestart} />
+      </View>
+    </View>
+  )
 };
 
 const styles = StyleSheet.create({
@@ -15,6 +35,33 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center'
+  },
+  imageContainer: {
+    width: 300,
+    height: 300,
+    borderRadius: 150,
+    borderWidth: 3,
+    borderColor: 'black',
+    overflow: 'hidden',
+    margin: 30,
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+  },
+  buttonContainer: {
+    margin: 20,
+  },
+  resultText: {
+    textAlign: 'center',
+    fontSize: 20,
+  },
+  highlight: {
+    color: colors.primary,
+    fontFamily: 'open-sans-bold',
+  },
+  resultsContainer: {
+    width: '80%'
   }
 });
 
