86ff0d81037fb90e6a78b24ac37f904dde5a3d9b
Tommi Haapanen <thaapane@student.jyu.fi>
2020-05-07 12:45:39 +0300

    Start screen looks good on smaller devices

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -5,7 +5,8 @@ import { View,
 				 Button,
 				 TouchableWithoutFeedback,
 				 Keyboard,
-				 Alert 
+				 Alert,
+				 Dimensions
 				} from 'react-native';
 
 import Card from '../components/Card';
@@ -103,8 +104,10 @@ const styles = StyleSheet.create({
 		fontFamily: 'open-sans-bold',
 	},
 	inputContainer: {
-		width: 300,
-		maxWidth: '80%',
+		width: '80%',
+		//maxWidth: '80%',
+		maxWidth: '95%',
+		minWidth: 300,
 		alignItems: 'center',
 	},
 	buttonContainer: {
@@ -114,7 +117,7 @@ const styles = StyleSheet.create({
 		paddingHorizontal: 15,
 	},
 	button: {
-		width: 100
+		width: Dimensions.get('window').width / 4,
 	},
 	input: {
 		width: 50,
